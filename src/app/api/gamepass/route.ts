ba209bb49bce7bc57cda789b98a887656ab84f66
Fagner Sales <fs.fagnersales@gmail.com>
2025-06-10 22:58:28 -0300

    add option for combining player avatar

--- src/app/api/gamepass/route.ts
+++ src/app/api/gamepass/route.ts
@@ -24,6 +24,14 @@ async function getIdFromUsername(username: string): Promise<{ id: number }> {
   return data.data[0]
 }
 
+async function getPlayerAvatar(userId: string, retries: number = 0): Promise<{ imageUrl: string }> {
+  const res = await fetch(`https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=${userId}&size=100x100&format=webp&isCircular=true`)
+  const data = await res.json()
+  const result = data.data[0]
+  if (result.state !== "Completed" && retries === 0) return getPlayerAvatar(userId, 1)
+  return result
+}
+
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
@@ -45,12 +53,17 @@ export async function GET(request: NextRequest) {
   }
 
   const sellableOnly = !!searchParams.get("sellableOnly")
+  const withPlayerAvatar = !!searchParams.get("withPlayerAvatar")
 
   const games = await getPlayerGames(id)
-  const allGamepasses = await Promise.all(games.map(game => getGamePasses(game.id))).then(passes => passes.flat())
-
-  return NextResponse.json(allGamepasses.filter(gamepass => {
+  const allGamepasses = await Promise.all(games.map(game => getGamePasses(game.id))).then(passes => passes.flat().filter(gamepass => {
     if (!sellableOnly) return true
     return typeof gamepass.price === "number"
   }))
+
+  if (withPlayerAvatar) {
+    return NextResponse.json({ gamepasses: allGamepasses, avatar: await getPlayerAvatar(id) })
+  }
+
+  return NextResponse.json(allGamepasses)
 }
