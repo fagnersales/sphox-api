ddf1cc64650db3d22e0cc24d971b9a05b90eb46c
Fagner Sales <fs.fagnersales@gmail.com>
2025-06-10 22:25:39 -0300

    AU AU AU sphqo au au au nina: MEOOOW

--- src/app/api/gamepass/route.ts
+++ src/app/api/gamepass/route.ts
@@ -1,12 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const username = request.nextUrl.searchParams.get("username")
-  if (!username) return NextResponse.json({ error: "Username not found in the search params" }, { status: 400 })
+async function getPlayerGames(userId: string): Promise<{ id: string }[]> {
+  const res = await fetch(`https://games.roblox.com/v2/users/${userId}/games`)
+  const data = await res.json()
+  return data.data
+}
 
-  return NextResponse.json({
-    name: username,
-    price: 1668,
-    url: "https://roblox.com/fagnersales/1668"
+async function getGamePasses(universeId: string): Promise<{ id: number, name: string, price: number | null }[]> {
+  const res = await fetch(`https://games.roblox.com/v1/games/${universeId}/game-passes?limit=100`)
+  const data = await res.json()
+  return data.data
+}
+
+async function getIdFromUsername(username: string): Promise<{ id: number }> {
+  const res = await fetch(`https://users.roblox.com/v1/usernames/users`, {
+    method: "POST",
+    body: JSON.stringify({
+      usernames: [username],
+      excludeBannedUsers: true
+    })
   })
+  const data = await res.json()
+  return data.data[0]
+}
+
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const userId = searchParams.get("userId")
+
+  const username = searchParams.get("username")
+  if (!userId && !username) return NextResponse.json({ error: "at least the userId or the username must exist in the search params" }, { status: 400 })
+
+  let id = userId;
+
+  if (username) {
+    const result = await getIdFromUsername(username)
+    if (!result) return NextResponse.json({ error: "UserId not found from the username" }, { status: 404 })
+    id = result.id.toString()
+  }
+
+  if (!id) {
+    return NextResponse.json({ error: "UserId not found" }, { status: 404 })
+  }
+
+  const sellableOnly = !!searchParams.get("sellableOnly")
+
+  const games = await getPlayerGames(id)
+  const allGamepasses = await Promise.all(games.map(game => getGamePasses(game.id))).then(passes => passes.flat())
+
+  return NextResponse.json(allGamepasses.filter(gamepass => {
+    if (!sellableOnly) return true
+    return typeof gamepass.price === "number"
+  }))
 }
